063d8d8193dc68a2bafb7566db6722794f9678b1
Moksha2511 <goyalmoksha2006@gmail.com>
2025-02-11 19:12:10 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -2,7 +2,7 @@
 #include<ctype.h>
 int main(){
     char ch;
-    scanf("%c", &c);
+    scanf("%c", &ch);
     if(isalpha(ch)){
         ch=tolower(ch);
         if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
