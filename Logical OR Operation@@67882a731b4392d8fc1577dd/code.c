dfe57de5c148051ee4dd3e67f9e77052886ad5f1
Moksha2511 <goyalmoksha2006@gmail.com>
2025-02-11 12:03:38 +0530

    Solution to Logical OR Operation

--- Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -2,7 +2,7 @@
 int main(){
     int a,b;
     if(scanf("%d %d", &a,&b)==2){
-        printf(a||b>0? "True\n":"False\n");
+        printf(a>0||b>0? "True\n":"False\n");
     }
     else{
         printf("True\n");
