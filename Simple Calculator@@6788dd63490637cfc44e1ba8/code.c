68af4be4495765623b7171684f685ae3f73a3ef6
Moksha Goyal <goyalmoksha2006@gmail.com>
2025-02-13 11:40:33 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,19 +4,19 @@ int main(){
     char operator;
     scanf("%d %d %c", &a,&b,&operator);
     switch(operator){
-        case 1:
+        case '+':
         result=a+b;
         printf("addition\n");
         break;
-        case 2:
+        case '-':
         result=a-b;
         printf("subtraction\n");
         break;
-        case 3:
+        case '*':
         result=a*b;
         printf("multiplication\n");
         break;
-        case 1:
+        case '/':
         result=a/b==0;
         printf("division\n");
         break;
